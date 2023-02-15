295b00e54e286dd3693632d4d2b342ace63728f9
Browinee <nihilitypeo@gmail.com>
2023-02-15 20:18:14 +0800

    feat: add keys.go

---
+++ dao/redis/keys.go
@@ -0,0 +1,8 @@
+package redis
+
+const (
+	KeyPrefix              = "ginhero:"
+	KeyPostTimeZSet        = "post:time"
+	KeyPostScoreZSet       = "post:score"
+	keyPostVotedZSetPrefix = "post:voted:"
+)
